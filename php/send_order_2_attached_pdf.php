87adb85f31b5fb16550779cc7621307316f6773d
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-04-09 14:09:41 +0200

    added pdf and html Mail Template to answer mail

--- php/send_order_2_attached_pdf.php
+++ php/send_order_2_attached_pdf.php
@@ -48,7 +48,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php'; 
+require 'phpmailer/src/PHPMailer.php';
 
 $mail = new PHPMailer(true);
 
@@ -57,66 +57,66 @@ $mail = new PHPMailer(true);
 
 require 'phpinvoice/phpinvoice.php';
 
-$order = new phpinvoice('purple','A4', '$'); // Put '&euro;' or 'CHF' etc. instead of '$'
+$order = new phpinvoice('purple','A4', 'CHF'); // Put '&euro;' or 'CHF' etc. instead of '$'
 
 $timestamp = time();
 $default_option = 'Select';
 $errors = '';
 
 // Setup Seller Details
-$seller_name = 'Seller Name';
-$seller_address = '123 Juanita Ave, Glendora, CA 91740, USA';
-$seller_notice = 'Contact data';
-$seller_company_name = 'Your Company';
+$seller_name = 'TEQLY GmbH';
+$seller_address = 'TECHNOPARK Aargua, Badenerstrasse 13, 5200 Brugg';
+$seller_notice = 'info@teqly.ch';
+$seller_company_name = 'Was macht hier Sinn?';
 
 /* Validate User Inputs
 ==================================== */
 
-// Name 
+// Name
 if ($_POST['username'] != '') {
-	
+
 	// Sanitizing
 	$_POST['username'] = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
 
 	if ($_POST['username'] == '') {
-		$errors .= 'Please enter a valid name.<br/>';
-	}
-}
-else { 
-	// Required to fill
-	$errors .= 'Please enter your name.<br/>';
-}
-
-// Email 
-if ($_POST['email'] != '') {
-
-	// Sanitizing 
-	$_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
-	// After sanitization validation is performed
-	$_POST['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-	
-	if($_POST['email'] == '') {
-		$errors .= 'Please enter a valid email address.<br/>';
+		$errors .= 'Geben Sie bitte einen Namen an.<br/>';
 	}
 }
 else {
 	// Required to fill
-	$errors .= 'Please enter your email address.<br/>';
+	$errors .= 'Geben Sie bitte einen Namen an.<br/>';
 }
 
-// Phone 
+// Email
+if ($_POST['email'] != '') {
+
+	// Sanitizing
+	$_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+	// After sanitization validation is performed
+	$_POST['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+
+	if($_POST['email'] == '') {
+		$errors .= 'Geben Sie bitte eine gültige E-Mail Adresse ein.<br/>';
+	}
+}
+else {
+	// Required to fill
+	$errors .= 'Geben Sie bitte eine E-Mail Adresse ein.<br/>';
+}
+
+// Phone
 if ($_POST['phone'] != '') {
 
 	// Sanitizing
 	$_POST['phone'] = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
 
 	// After sanitization validation is performed
-	$pattern_phone = array('options'=>array('regexp'=>'/^\+{1}[0-9]+$/'));
+	$pattern_phone = array('options'=>array('regexp'=>'/^\+{41}[0-9]+$/'));
 	$_POST['phone'] = filter_var($_POST['phone'], FILTER_VALIDATE_REGEXP, $pattern_phone);
-	
+
 	if($_POST['phone'] == '') {
-		$errors .= 'Please enter a valid phone number like: +363012345<br/>';
+		$errors .= 'Geben Sie bitte eine gültige Telefonnummer im Format +41 12 345 67 89 ein.<br/>';
 	}
 }
 
@@ -125,9 +125,9 @@ if ($_POST['address'] != '') {
 
 	// Sanitizing
 	$_POST['address'] = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-	
+
 	if($_POST['address'] == '') {
-		$errors .= 'Please enter a valid address.<br/>';
+		$errors .= 'Bitte geben Sie eine gültige Adresse ein.<br/>';
 	}
 }
 
@@ -136,9 +136,9 @@ if ($_POST['message'] != '') {
 
 	// Sanitizing
 	$_POST['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-	
+
 	if($_POST['message'] == '') {
-		$errors .= 'Please enter a valid message.<br/>';
+		$errors .= 'Schreiben Sie bitte ein gültige Nachricht.<br/>';
 	}
 }
 
@@ -146,12 +146,12 @@ if ($_POST['message'] != '') {
 ==================================== */
 
 function sanitizePostTitle($postName, $invalidMessage) {
-	
+
 	if ($_POST[$postName] != '') {
-		
+
 		// Sanitizing
 	  	$_POST[$postName] = filter_var($_POST[$postName], FILTER_SANITIZE_STRING);
-		  
+
 		if ($_POST[$postName] == '') {
 			return $invalidMessage . '<br/>';
 	  	}
@@ -165,30 +165,30 @@ function validatePostValue($postValue, $invalidMessage) {
 	if ($_POST[$postValue] != '') {
 
 		// Sanitizing
-		$_POST[$postValue] = filter_var($_POST[$postValue], FILTER_SANITIZE_STRING);		
+		$_POST[$postValue] = filter_var($_POST[$postValue], FILTER_SANITIZE_STRING);
 
 		// After sanitization validation is performed
 		if ($_POST[$postValue] != '0') {
 			$_POST[$postValue] = filter_var($_POST[$postValue], FILTER_VALIDATE_FLOAT);
-		}		
+		}
 
 		if ($_POST[$postValue] == '') {
 			return $invalidMessage . '<br/>';
 		}
 
-	}	
+	}
 	return '';
 }
 
 function validatePostTotal($postValue, $invalidMessage) {
 
 	if ($_POST[$postValue] != '') {
-		
-		if (!filter_var($_POST[$postValue], FILTER_VALIDATE_FLOAT)) {			
+
+		if (!filter_var($_POST[$postValue], FILTER_VALIDATE_FLOAT)) {
 			return $invalidMessage . '<br/>';
 		}
 
-	}	
+	}
 	return '';
 }
 
@@ -222,37 +222,37 @@ if (!$errors) {
 	$customer_name = $_POST['username'];
 	$customer_mail = $_POST['email'];
 	$customer_phone = $_POST['phone'];
-	$customer_address = $_POST['address'];	
+	$customer_address = $_POST['address'];
 	$customer_message = $_POST['message'];
-	
+
 	// Handle empty customer inputs
 	if (empty($customer_phone)) {
-		$customer_phone = 'Was not provided.';
+		$customer_phone = 'Keine Telefonnummer hinterlasssen';
 	}
 	if (empty($customer_address)) {
-		$customer_address = 'Address: was not provided.';
+		$customer_address = 'Keine Adresse hinterlasssen';
 	}
 	if (empty($customer_message)) {
-		$customer_message = 'Was not provided.';
+		$customer_message = 'Keine Bermerkungen hinterlasssen';
 	}
 
 	// Option Group 1
 	$selected_option1 = $_POST['option1Single'];
 	$selected_option1_title = $_POST['option1Title'];
-	$selected_option1_price = $_POST['option1Price'];	
-	$option1_qty = $_POST['optionGroup1Qty'];	
+	$selected_option1_price = $_POST['option1Price'];
+	$option1_qty = $_POST['optionGroup1Qty'];
 
 	// Option Group 2
 	$selected_option2 = $_POST['option2Single'];
 	$selected_option2_title = $_POST['option2Title'];
 	$selected_option2_price = $_POST['option2Price'];
-	$option2_qty = $_POST['optionGroup2Qty'];	
+	$option2_qty = $_POST['optionGroup2Qty'];
 
 	// Option Group 3
 	$selected_option3 = $_POST['option3Single'];
 	$selected_option3_title = $_POST['option3Title'];
 	$selected_option3_price = $_POST['option3Price'];
-	$option3_qty = $_POST['optionGroup3Qty'];	
+	$option3_qty = $_POST['optionGroup3Qty'];
 
 	// Extra Option 1
 	$extra_option_1 = $_POST['extraOption1'];
@@ -260,14 +260,14 @@ if (!$errors) {
 
 	// Extra Option 2
 	$extra_option_2 = $_POST['extraOption2'];
-	$extra_option_2_title = $_POST['extraOption2Title'];	
+	$extra_option_2_title = $_POST['extraOption2Title'];
 
 	/* Order Generation
 	==================================== */
-	
-	// Header Settings	
-	$order->setLogo('phpinvoice/templates/purple/logo.png');	
-	$order->setReference('ORDER-'.$timestamp);
+
+	// Header Settings
+	$order->setLogo('phpinvoice/templates/purple/logo.png');
+	$order->setReference('ANFRAGE-'.$timestamp);
 	$order->setDate(date('M dS, Y',time()));
 
 	$order->setFrom(array($seller_name, $seller_address));
@@ -282,7 +282,7 @@ if (!$errors) {
 	}
 	if (isset($selected_option3)) {
 		$order->addItem($selected_option3_title, '', $option3_qty, "0%", $selected_option3_price, false);
-	}	
+	}
 
 	// Adding Extra Items (name, description, amount, vat, price, discount) only if selected
 	if (isset($extra_option_1)) {
@@ -292,52 +292,52 @@ if (!$errors) {
 		$order->addItem($extra_option_2_title, 'Extra Option 2 description', 1, "0%", 70, false);
 	}
 
-	// Add Totals	
-	$order->addTotal("Total", $order->items_total);  	  	
+	// Add Totals
+	$order->addTotal("Total", $order->items_total);
 
 	// Add Title
 	$order->addTitle($seller_notice);
 
 	// Add Paragraph
-	$order->addParagraph('Email: ' . $customer_mail);
-	$order->addParagraph('Phone: ' . $customer_phone . '<br/><br />');
-	$order->addParagraph('<strong>Message:</strong> ' . '<br />' . $customer_message);
+	$order->addParagraph('E-Mail: ' . $customer_mail);
+	$order->addParagraph('Telefon ' . $customer_phone . '<br/><br />');
+	$order->addParagraph('<strong>Nachricht:</strong> ' . '<br />' . $customer_message);
 
 	// Set Footer Note
 	$order->setFooternote('Created with <a href="https://ultimatewebsolutions.net/costy/" target="_blank">Costy 2.0</a>');
 
 	// Render
-	$order->render('phpinvoice/pdf/order_' . $timestamp . '.pdf','F');	
+	$order->render('phpinvoice/pdf/order_' . $timestamp . '.pdf','F');
 
 	/* Mail Sending
 	==================================== */
-	
-	try {    
 
-		// Set Sender	
-		$mail->setFrom('noreply@yourdomain.com', 'Costy');
+	try {
 
-		// Set Reply-to Address	
-		$mail->addReplyTo('replyto@yourdomain.com', 'Costy');
+		// Set Sender
+		$mail->setFrom('noreply@teqly.ch', 'TEQLY');
 
-		// Set Recipients	
-		$mail->addAddress('websolutions.ultimate@gmail.com', 'Ultimate Websolutions');
+		// Set Reply-to Address
+		$mail->addReplyTo('info@teqly.ch', 'TEQLY');
+
+		// Set Recipients
+		$mail->addAddress('info@teqly.ch', 'TEQLY GmbH');
 		$mail->addAddress($customer_mail, $customer_name_formatted);
 
 		// Set Subject
-		$mail->Subject = 'Order Request from Costy';
+		$mail->Subject = 'Ihre Anfrage via TEQLY | Rental';
 
 		// Get HTML Mail Template
 		$email_html = file_get_contents('phpmailer/email.html');
 
-		// Set HTML Content	
+		// Set HTML Content
 		$body = str_replace(array('customerName'), array($customer_name), $email_html);
-		$mail->MsgHTML($body);	
-		
+		$mail->MsgHTML($body);
+
 		// Attach Invoice
 		$file = 'phpinvoice/pdf/order_' . $timestamp . '.pdf';
 		$mail->addAttachment($file);
-		
+
 		$mail->send();
 
 	} catch (Exception $e) {
@@ -353,7 +353,7 @@ if (!$errors) {
 			unlink($file);
 		}
 	}
-	
+
 	// Success Page
 	echo '<div id="success">';
     echo '<div class="icon icon-order-success svg">';
@@ -363,9 +363,9 @@ if (!$errors) {
 	echo '<path d="M17.417,37.778l9.93,9.909l25.444-25.393" style="stroke-dasharray:50px, 50px; stroke-dashoffset: 0px;"></path>';
     echo '</g>';
 	echo '</svg>';
-	echo '</div>';    
-	echo '<h4>Thank you for your order.</h4>';
-	echo '<small>Check your mailbox.</small>';
+	echo '</div>';
+	echo '<h4>Vielen Dank für Ihre Anfrage. Sie wurde erfolgreich übermittelt.</h4>';
+	echo '<small>Wir haben Ihnen eine Bestätigungsnachricht geschickt. Überprüfen Sie Ihre Mailbox.</small>';
 	echo '</div>';
 	echo '<script src="../js/redirect.js"></script>';
 
@@ -373,9 +373,9 @@ if (!$errors) {
 
 	// Error Page
 	echo '<div style="color: #e9431c">' . $errors . '</div>';
-	echo '<div id="success">';    
-	echo '<h4>Something went wrong.</h4>';
-	echo '<a class="animated-link" href="../index.html">Go Back</small>';
+	echo '<div id="success">';
+	echo '<h4>Ups, da hatt etwas nicht geklappt!</h4>';
+	echo '<a class="animated-link" href="../index.html">Zurück zur Startseite</small>';
 	echo '</div>';
 
 }
