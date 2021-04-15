12b26642fa6e1ce4b35d4b13391192a43894a795
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-04-15 19:18:22 +0200

    outsourcing of chat on index.php

--- index.php
+++ index.php
@@ -213,19 +213,9 @@
 <!-- Main -->
 <script src="js/main.js"></script>
 
-<!-- Zammad Chat -->
-<script src="https://support.teqly.app/assets/chat/chat.min.js"></script>
-<script>
-    $(function() {
-        new ZammadChat({
-            title: '<strong>Wir beantworten Ihre Fragen gerne im Chat!</strong>',
-            background: '#DD356E',
-            fontSize: '12px',
-            flat: true,
-            chatId: 1
-        });
-    });
-</script>
+<?php
+include('shared/zammadchat.php')
+?>>
 
 </body>
 </html>
---
+++ shared/zammadchat.php
@@ -0,0 +1,13 @@
+<!-- Zammad Chat -->
+<script src="https://support.teqly.app/assets/chat/chat.min.js"></script>
+<script>
+    $(function() {
+        new ZammadChat({
+            title: '<strong>Wir beantworten Ihre Fragen gerne im Chat!</strong>',
+            background: '#DD356E',
+            fontSize: '12px',
+            flat: true,
+            chatId: 1
+        });
+    });
+</script>
