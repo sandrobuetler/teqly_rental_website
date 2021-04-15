a730fbcf1b3b10e8cb159d30ed347b041018aa36
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-04-15 19:30:20 +0200

    shortend link to index.html in navigation.php

--- shared/navigation.php
+++ shared/navigation.php
@@ -6,7 +6,7 @@
                 <IMG SRC="../images/teqly_logo_back.png" ALT="TEQLY Logo" HEIGHT=50 align="left">
                 <div class="col-xs-10 text-right menu-1">
                     <ul style="font-family: 'Source Code Pro' !important;">
-                        <li style="margin: -10px" class="<?php $r = ($uri == "/index.php") ? 'active' : ''; echo $r ?>" style="margin: -10px !important;"><a href="../index.php">Start</a></li>
+                        <li style="margin: -10px" class="<?php $r = ($uri == "/") ? 'active' : ''; echo $r ?>" style="margin: -10px !important;"><a href="../">Start</a></li>
                         <li style="margin: -10px" class="<?php $r = ($uri == "/vorteile.php") ? 'active' : ''; echo $r ?>"><a href="../vorteile.php">Vorteile</a></li>
                         <li style="margin: -10px" class="has-dropdown <?php $r = ($uri == "/hardware.php" || $uri == "/hw_desktop.php" || $uri == "/hw_laptop.php" || $uri == "/hw_server.php" ) ? 'active' : ''; echo $r ?>">
                             <a href="../hardware.php">Hardware</a>
