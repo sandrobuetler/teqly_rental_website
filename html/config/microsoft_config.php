ebfea246c2e656383d41e5000f2c11d14e2ed112
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-04-15 15:33:06 +0200

    updated footer with footer.php on all sites

--- html/config/microsoft_config.php
+++ html/config/microsoft_config.php
@@ -337,72 +337,9 @@
         </div>
     </div>
 
-    <footer id="fh5co-footer" role="contentinfo">
-        <div class="container">
-            <div class="row row-pb-md">
-                <div class="col-md-4 fh5co-widget">
-                    <h4>TEQLY | Rental</h4>
-                    <p>Dieses Angebot ist eine Dienstleistung der TEQLY GmbH, dem innovativen KMU IT-Partner aus dem
-                        Aargau</p>
-                </div>
-                <div class="col-md-2 col-md-push-1 fh5co-widget">
-                    <h4>Navigation</h4>
-                    <ul class="fh5co-footer-links">
-                        <li><a href="../../index.php">Start</a></li>
-                        <li><a href="../../vorteile.php">Vorteile</a></li>
-                        <li><a href="../../hardware.php">Hardware</a></li>
-                        <li><a href="../../software.php">Software</a></li>
-                        <li><a href="../../swissmade.php">Swiss Made</a></li>
-                        <li><a href="../../contact.php">Kontakt</a></li>
-                        <li><a href="../../calculator_yearly.php">Preisrechner</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-2 col-md-push-1 fh5co-widget">
-                    <h4>Mehr TEQLY</h4>
-                    <ul class="fh5co-footer-links">
-                        <li><a href="https://teqly.ch/">Website</a></li>
-                        <li><a href="https://teqly.ch/blog.html">Blog</a></li>
-                        <li><a href="https://teqly.ch/services.html">Services</a></li>
-                        <li><a href="https://teqly.ch/produkte.html">Produkte</a></li>
-                        <li><a href="https://support.teqly.app">Support Plattform</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 col-md-push-1 fh5co-widget">
-                    <h4>Kontakt</h4>
-                    <ul class="fh5co-footer-links">
-                        <li>TEQLY GmbH <br> TECHNOPARK&copy; Aargau <br> Badenerstrasse 13 <br> 5200 Brugg</li>
-                        <li><a href="tel://+41625614002">+41 62 561 40 02</a></li>
-                        <li><a href="mailto:info@teqly.ch">info@teqly.ch</a></li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="row copyright">
-                <div class="col-md-12 text-center">
-                    <p>
-                        <small class="block">&copy; 2021 gebaut und gehostet von TEQLY</small>
-                        <small class="block"><a href="https://teqly.ch/legal.html#impressum"
-                                                target="_blank">Impressum</a> <a
-                                href="https://teqly.ch/legal.html#datenschutz" target="_blank">Datenschutz</a> <a
-                                href="https://teqly.ch/legal.html#agb" target="_blank">AGB's</a> </small>
-                    </p>
-                    <p>
-                    <ul class="fh5co-social-icons">
-                        <li><a href="https://www.instagram.com/teqly/" target="_blank"><i
-                                class="icon-instagram"></i></a></li>
-                        <li><a href="https://www.linkedin.com/company/teqly" target="_blank"><i
-                                class="icon-linkedin2"></i></a></li>
-                        <li><a href="mailto:info@teqly.ch" target="_blank"><i class="icon-mail"></i></a></li>
-                    </ul>
-                    </p>
-                </div>
-            </div>
-
-        </div>
-    </footer>
+    <?php
+    include('../../footer.php');
+    ?>
 </div>
 
 <div class="gototop js-top">
