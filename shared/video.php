62b41b64539adff5771e843f6e344d2aff7d3b8d
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2021-06-01 14:18:06 +0200

    ios preview hack

--- shared/video.php
+++ shared/video.php
@@ -3,8 +3,8 @@
         <div class="services-padding">
             <h2>How to TEQLY | Rental?</h2>
             <p>Lernen Sie unsere Musterkundin Julia kennen. Sehen Sie im Video, wie Julia dank TEQLY | Rental Zeit und Geld spart ohne auf einen top Service verzichten zu müssen!</p>
-            <video width="80%" height="80%" controls preload="metadata" poster="Here is the Poster" style="display: block; margin: 0 auto">
-                <source src="videos/teqly_rental.mp4" type="video/mp4">
+            <video width="80%" height="80%" controls preload="metadata" style="display: block; margin: 0 auto">
+                <source src="videos/teqly_rental.mp4#t=0.0001" type="video/mp4">
                 Ihr Browser unterstützt leider keine Videowiedergabe!
             </video>
         </div>
