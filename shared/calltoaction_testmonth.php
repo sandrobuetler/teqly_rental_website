c04f2c6be300576517a869b5c436233bb7fe661a
Sandro Bütler <sandro.buetler@students.fhnw.ch>
2022-04-19 15:51:23 +0200

    Update calltoaction_testmonth.php

--- shared/calltoaction_testmonth.php
+++ shared/calltoaction_testmonth.php
@@ -1,8 +1,8 @@
 <div id="fh5co-started">
-    <div class="overlay"></div>
+    <div class="overlay" style="background: #DD356E"></div>
     <div class="container">
         <div class="row animate-box">
-            <div class="col-md-8 col-md-offset-2 text-center fh5co-heading" style="color: #DD356E">
+            <div class="col-md-8 col-md-offset-2 text-center fh5co-heading" >
                 <h2>Noch Fragen?<br> Stellen Sie sie uns!</h2>
                 <p>Wir sind für Sie da! Für Fragen, Bestellungen oder Anregungen erreichen Sie uns via Mail, Chat
                     und Telefon.</p>
