d273b6661643605f6e4776da345aa2ff68bf3e1e
jannik1995 <50104987+jannik1995@users.noreply.github.com>
2022-04-21 15:41:54 +0200

    Cookies und styling

--- shared/cookie.php
+++ shared/cookie.php
@@ -1,9 +1,22 @@
+<!-- Cookie Consent by TermsFeed Privacy Generator https://www.TermsFeed.com -->
+<script type="text/javascript" src="//www.termsfeed.com/public/cookie-consent/4.0.0/cookie-consent.js" charset="UTF-8"></script>
+<script type="text/javascript" charset="UTF-8">
+    document.addEventListener('DOMContentLoaded', function () {
+        cookieconsent.run({"notice_banner_type":"headline","consent_type":"express","palette":"light","language":"de","page_load_consent_levels":["strictly-necessary"],"notice_banner_reject_button_hide":false,"preferences_center_close_button_hide":false,"page_refresh_confirmation_buttons":false});
+    });
+</script>
+
+<!-- Google Analytics -->
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2FHN7FDZC3"></script>
-<script>
+<script type="text/plain" cookie-consent="tracking" async src="https://www.googletagmanager.com/gtag/js?id=G-2FHN7FDZC3"></script>
+<script type="text/plain" cookie-consent="tracking">
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-2FHN7FDZC3');
 </script>
+<!-- end of Google Analytics-->
+
+<noscript>ePrivacy and GPDR Cookie Consent management by <a href="https://www.termsfeed.com/" rel="nofollow noopener">TermsFeed Privacy Generator</a></noscript>
+<!-- End Cookie Consent by TermsFeed Privacy Generator https://www.TermsFeed.com -->
